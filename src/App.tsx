2379c744c3cd455307035efddb4ba5b110d98367
Odin Kammerloher <kammerloher.andreas@gmail.com>
2024-08-06 12:49:27 +0200

    Add mantine and react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { AppShell, Burger, Button, Text, Title } from "@mantine/core"
+import { useDisclosure } from "@mantine/hooks"
+import { useQuery } from "@tanstack/react-query"
+import "./App.css"
+import viteLogo from "/vite.svg"
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [opened, { toggle }] = useDisclosure()
+    const { data, isFetching, error } = useQuery({ queryKey: ["ditto"], queryFn: fetchPokemon })
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    console.log(data)
+    return (
+        <AppShell
+            header={{ height: 80 }}
+            navbar={{
+                width: 300,
+                breakpoint: "sm",
+                collapsed: { mobile: !opened },
+            }}
+            padding="md"
+        >
+            <AppShell.Header p={"lg"}>
+                <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+                <img src={viteLogo} className="logo" alt="Vite logo" />
+            </AppShell.Header>
+
+            <AppShell.Navbar p="md">
+                <Text>NavBar 1</Text>
+                <Text>NavBar 2</Text>
+                <Text>NavBar 3</Text>
+                <Button>Test</Button>
+            </AppShell.Navbar>
+
+            <AppShell.Main>
+                <Title>Mantine Template</Title>
+
+                {isFetching ? <Text>Fetching...</Text> : null}
+                {error ? <Text>Error: {error.message}</Text> : null}
+                {data ? <Text>Ditto's moves as fetched from API: {data.abilities.map((m) => m.ability.name).join(", ")}</Text> : null}
+            </AppShell.Main>
+
+            <AppShell.Aside p="md">Aside</AppShell.Aside>
+            <AppShell.Footer p="md">Footer</AppShell.Footer>
+        </AppShell>
+    )
+}
+
+type Pokemon = {
+    abilities: { ability: { name: string } }[]
+}
+
+async function fetchPokemon(): Promise<Pokemon> {
+    const url = "https://pokeapi.co/api/v2/pokemon/ditto"
+
+    const res = await fetch(url)
+    return res.json()
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+import "@mantine/core/styles.css"
+import { createTheme, MantineProvider } from "@mantine/core"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const theme = createTheme({})
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <MantineProvider theme={theme}>
+            <QueryClientProvider client={queryClient}>
+                <App />
+            </QueryClientProvider>
+        </MantineProvider>
+    </React.StrictMode>
 )
